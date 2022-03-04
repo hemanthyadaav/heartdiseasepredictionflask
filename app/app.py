a0512110bf7db859df6e38c5d38d6f75a3e2c1e9
U Hemantha Kumar <64372575+hemanthyadaav@users.noreply.github.com>
2022-03-04 22:41:04 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from tokenize import String
 from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
@@ -10,9 +12,58 @@ app = Flask(__name__,template_folder='templates')
 def hello_world():
      request_type_str = request.method
      if request_type_str == "GET":
-        return render_template("index.html", output = "Predict the Output by inputting the values and Enter Submit")
+        return render_template("index.html", output = "Enter the values and hit Submit to view results")
      else: 
-        text = request.form["text"]
+        # text = request.form["text"]
+
+        text = ""
+
+        age = request.form["age"]
+        text = text + ","
+
+        anaemia = request.form["anaemia"]
+        text = text + anaemia
+        text = text + ","
+
+        creatinine_phosphokinase = request.form["creatinine_phosphokinase"]
+        text = text + creatinine_phosphokinase
+        text = text + ","
+        
+        diabetes = (request.form["diabetes"])
+        text = text + diabetes
+        text = text + ","
+        
+        ejection_fraction = (request.form["ejection_fraction"])
+        text = text + ejection_fraction
+        text = text + ","
+         
+        high_blood_pressure = (request.form["high_blood_pressure"])
+        text = text + high_blood_pressure
+        text = text + ","
+         
+        platelets = (request.form["platelets"])
+        text = text + platelets
+        text = text + ","
+         
+        serum_creatinine = (request.form["serum_creatinine"])
+        text = text + serum_creatinine
+        text = text + ","
+         
+        serum_sodium = (request.form["serum_sodium"])
+        text = text + serum_sodium
+        text = text + ","
+         
+        sex = (request.form["sex"])
+        text = text + sex
+        text = text + ","
+         
+        smoking = (request.form["smoking"])
+        text = text + smoking
+        text = text + ","
+         
+        time = (request.form["time"])
+        text = text + time
+
         model = load("app/model.joblib")
         np_arr = floats_string_to_np_arr(text)
 
